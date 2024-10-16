d9fe271763be903ff6cb066be0c612a4c03a2d3c
Rohan-218 <ntomar313@gmail.com>
2024-10-16 11:57:28 +0530

    KaIdO

--- server/src/controllers/teacherUpdateMarksController.js
+++ server/src/controllers/teacherUpdateMarksController.js
@@ -1,10 +1,14 @@
 const teacherUpdateMarksService = require('../services/teacherUpdateMarksService');
+const teacherPostMarksService = require('../services/teacherPostMarksService');
+const sendEmailNotification = require('../utils/emailservice');
 
 // Update marks
 const updateMarks = async (req, res) => {
   try {
     const { exam_id, marks } = req.body;
     const usertype = req.user.user_type;
+
+    const studentIds = marks.map(mark => mark.student_id);
 
     if (usertype !== 2) {
       return res.status(403).json({ message: 'Unauthorized: Not a teacher!' });
@@ -33,9 +37,28 @@ const updateMarks = async (req, res) => {
 
     // Prepare marks data
     const marksData = await teacherUpdateMarksService.prepareMarksData(marks, exam_id);
+    const subjectName = marksData.map(mark => mark.subject_name );
 
-    // Update marks in the database
     await teacherUpdateMarksService.updateMarks(marksData);
+
+    const studentEmails = await teacherPostMarksService.getUserId(studentIds);
+    console.log('Fetched student emails:', studentEmails);
+ 
+     // Now that we have student emails, we can send them notifications
+     const emailList = studentEmails.map(student => student.email);
+
+      // Now that we have student emails, we can send them notifications
+      try {
+        const text = `Dear Student,\n\nMarks for ${subjectName[0]} have been updated.\n\nRegards,\nXYZ University`;
+        const emailResponse = await sendEmailNotification(emailList, text);
+        if (emailResponse) {
+          console.log('Emails sent successfully:', emailResponse);  // Log the successful response from SendGrid
+        }
+      } catch (error) {
+        // Log detailed error from SendGrid
+        console.error('Error sending email:', error.response ? error.response.body.errors : error.message);
+        return res.status(500).json({ message: 'Error sending email notifications' });
+      }
 
     res.status(200).json({ message: 'Marks updated successfully' });
   } catch (error) {
