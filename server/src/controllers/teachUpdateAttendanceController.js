d837b98d6d43d9cdf668f1df50038dce12bcbd38
Rohan-218 <ntomar313@gmail.com>
2024-10-17 12:10:26 +0530

    Attendance update activity

--- server/src/controllers/teachUpdateAttendanceController.js
+++ server/src/controllers/teachUpdateAttendanceController.js
@@ -2,11 +2,16 @@
 const sendEmailNotification = require('../utils/emailservice');
 const userService = require('../services/userService');
 const attendanceService = require('../services/teachUpdateAttendanceService');
+const { insertActivity } = require('../utils/activityService');
 
 const updateAttendance = async (req, res) => {
   try {
     const { subjectCode, attendanceDate, lecture, attendanceList } = req.body;
-    
+    const {user_type, user_id} = req.user;
+
+    if (user_type !== 2) {
+      return res.status(403).json({ message: 'Unauthorized: Not a teacher!' });
+    }
     // Validate the request body
     if (!subjectCode || !attendanceDate || !lecture || !attendanceList) {
       return res.status(400).json({ error: 'Missing required fields' });
@@ -17,23 +22,24 @@ const updateAttendance = async (req, res) => {
     const studentIds = await attendanceService.getStudentId(attendanceList);
     const extractedStudentIds = studentIds.map(student => student.student_id);
 
-    const studentEmails = await userService.getUserId(extractedStudentIds); 
+    const studentData = await userService.getUserId(extractedStudentIds); 
      // Now that we have student emails, we can send them notifications
-     const emailList = studentEmails.map(student => student.email);
-   
-   // Now that we have student emails, we can send them notifications
-   try {
-    const text = `Dear Student,\n\nAttendance for subject code - ${subjectCode} have been updated.\n\nRegards,\nXYZ University`;
-    const subject = `Attendance Updated!`;
-    const emailResponse = await sendEmailNotification(emailList, text, subject);
-    if (emailResponse) {
-      console.log('Emails sent successfully:', emailResponse);  // Log the successful response from SendGrid
+    const emailList = studentData.map(student => student.email);
+    const student = studentData.map(student => student.student_name);
+    // Now that we have student emails, we can send them notifications
+    try {
+      const text = `Dear Student,\n\nAttendance for subject code - ${subjectCode} have been updated.\n\nRegards,\nXYZ University`;
+      const subject = `Attendance Updated!`;
+      insertActivity( user_id, 'Attendance updated', `Attendance of ${student} in ${subjectCode} for ${attendanceDate} lecture- ${lecture} have been updated.`);
+      const emailResponse = await sendEmailNotification(emailList, text, subject);
+      if (emailResponse) {
+        console.log('Emails sent successfully:', emailResponse);  // Log the successful response from SendGrid
+      }
+    } catch (error) {
+      // Log detailed error from SendGrid
+      console.error('Error sending email:', error.response ? error.response.body.errors : error.message);
+      return res.status(500).json({ message: 'Error sending email notifications' });
     }
-  } catch (error) {
-    // Log detailed error from SendGrid
-    console.error('Error sending email:', error.response ? error.response.body.errors : error.message);
-    return res.status(500).json({ message: 'Error sending email notifications' });
-  }
 
     return res.status(200).json({ message: 'Attendance updated successfully' });
   } catch (error) {
