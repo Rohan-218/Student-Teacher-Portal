7de9deee59e2acf432da3430e03738e51f38e82c
Rohan-218 <ntomar313@gmail.com>
2024-10-17 16:53:09 +0530

    Admin teacher Management

--- server/src/controllers/createStudentController.js
+++ server/src/controllers/createStudentController.js
@@ -6,13 +6,13 @@ exports.createStudent = async (req, res) => {
     const studentData = req.body;
     const  { user_id, user_type } = req.user;
 
-    if (user_type !== 0 && userType !== 3) {
+    if (user_type !== 0 && user_type !== 3) {
       return res.status(403).json({ message: 'Access denied. Only admins can get admin data.' });
     }
     const result = await createStudentService.createStudent(studentData);
     const { name } = studentData;
 
-    insertActivity( user_id, 'New Student Created', `New Student - ( ${name} ) have been added.`);
+    insertActivity( user_id, 'New Student Added', `New Student - ( ${name} ) have been added.`);
     res.status(201).json({ message: 'Student created successfully', student: result });
   } catch (error) {
     res.status(500).json({ error: error.message });
