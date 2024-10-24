ef2f487dd29c7f09bf6613942dca0c5124633412
Rohan-218 <ntomar313@gmail.com>
2024-10-25 00:26:22 +0530

    code cleanup

--- client_1/src/App.jsx
+++ client_1/src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import Header from './components/header/index.jsx';
-import Sidebar from './components/sidebar/index.jsx';
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Sidebar />
     </>
   )
 }
