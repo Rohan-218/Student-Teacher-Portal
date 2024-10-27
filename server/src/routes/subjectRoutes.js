abaae3bf884e2497910ac8c76ea38755764c588d
Rohan-218 <ntomar313@gmail.com>
2024-10-27 13:46:31 +0530

    subject route middleware

--- server/src/routes/subjectRoutes.js
+++ server/src/routes/subjectRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getSubjects, createSubject, getSubjectCount, updateSubjectIsActive } = require('../controllers/subjectController');
+const { admin } = require('../middleware/isAllowed');
 
-router.get('/', getSubjects);
-router.get('/count', getSubjectCount);
-router.post('/create', createSubject);
-router.put('/update', updateSubjectIsActive);
+router.get('/', admin, getSubjects);
+router.get('/count', admin, getSubjectCount);
+router.post('/create', admin, createSubject);
+router.put('/update', admin, updateSubjectIsActive);
 
 module.exports = router;
