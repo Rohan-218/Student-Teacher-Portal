7f28b2f330579fd64a1c09ca8bb5d567aef25861
Rohan-218 <ntomar313@gmail.com>
2024-10-27 12:57:21 +0530

    exam route middleware

--- server/src/routes/examRoutes.js
+++ server/src/routes/examRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getExam, createExam, updateExamIsActive } = require('../controllers/examController');
+const { admin } = require('../middleware/isAllowed');
 
-router.get('/', getExam);
-router.post('/create', createExam);
-router.put('/update', updateExamIsActive);
+router.get('/', admin, getExam);
+router.post('/create', admin, createExam);
+router.put('/update', admin, updateExamIsActive);
 
 module.exports = router;
