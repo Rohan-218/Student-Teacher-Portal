e8b5e55a1c71050f6e24ec157cdd5b8ae769c6bf
Rohan-218 <ntomar313@gmail.com>
2024-10-27 12:49:27 +0530

    branch route middleware

--- server/src/routes/branchRoutes.js
+++ server/src/routes/branchRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getBranch, createBranch, getBranchCount, getBranchStudentCount, updateBranchIsActive } = require('../controllers/branchController');
+const { admin } = require('../middleware/isAllowed');
 
-router.get('/', getBranch);
-router.get('/count', getBranchCount);
-router.get('/student-count', getBranchStudentCount);
-router.post('/create', createBranch);
-router.put('/update', updateBranchIsActive);
+router.get('/', admin, getBranch);
+router.get('/count', admin, getBranchCount);
+router.get('/student-count', admin, getBranchStudentCount);
+router.post('/create', admin, createBranch);
+router.put('/update', admin, updateBranchIsActive);
 
 module.exports = router;
