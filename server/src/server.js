e6c202050d4d12fb4cafaa71d8e50318c60d6a0f
Rohan-218 <ntomar313@gmail.com>
2024-10-30 14:59:47 +0530

    ...

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const app = require('./app.js');
-// Set the port
-const PORT = 3000;
 
-// Start the server
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
