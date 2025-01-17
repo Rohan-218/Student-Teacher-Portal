104cf1ea6be780bcb54440aff061e494fc856ec7
Rohan-218 <ntomar313@gmail.com>
2025-01-17 14:49:29 +0530

    local db connect

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -1,29 +1,25 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,               // SSL is required
-      rejectUnauthorized: false,    // Disable certificate verification (use with caution)
-    },
-  },
+  logging: false, 
   pool: {
     max: 5,
-    min: 0,
-    acquire: process.env.DB_TIMEOUT * 1000,
-    idle: 10000,
+    min: 0, 
+    acquire: (process.env.DB_TIMEOUT || 30) * 1000, 
+    idle: 10000, 
   },
 });
 
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
   })
-  .catch(err => {
+  .catch((err) => {
     console.error('Unable to connect to the database:', err);
   });
 
