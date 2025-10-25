9d19149907ef20472407c85e0bfe3e943a4f083e
Rohan-218 <ntomar313@gmail.com>
2025-10-25 17:27:41 +0530

    db config for ssl  render

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -1,18 +1,29 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  logging: false, 
-  pool: {
-    max: 5,
-    min: 0, 
-    acquire: (process.env.DB_TIMEOUT || 30) * 1000, 
-    idle: 10000, 
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: (process.env.DB_TIMEOUT || 30) * 1000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,        // Use SSL
+        rejectUnauthorized: false, // Accept self-signed certificates
+      },
+    },
+  }
+);
 
 sequelize
   .authenticate()
